30ce8533f7619557ac71f7c7c30060d85bacb621
elliott-home-pc <elliott.wagener@mude.com.au>
2021-02-14 18:00:11 +1100

    Change TeamMember to Project

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,7 +3,7 @@ const createPages = require('./create/createPages')
 
 module.exports.createPagesStatefully = async gatsbyUtilities => {
   // Assuming in your wordpress site you are registering a post type with
-  // graphql_single_name = teamMember
-  await createPages({ postTypes: ['Page', 'TeamMember'], gatsbyUtilities, })
+  // graphql_single_name = project
+  await createPages({ postTypes: ['Page', 'Project'], gatsbyUtilities, })
 }
 
