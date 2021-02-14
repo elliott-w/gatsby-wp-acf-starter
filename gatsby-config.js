8cacd2ec844dc0c6bd3cd5afefb6da2d910155ba
elliott-home-pc <elliott.wagener@mude.com.au>
2021-02-14 17:59:32 +1100

    Update wp url

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       resolve: `gatsby-source-wordpress`,
       options: {
-        url: process.env.WPGRAPHQL_URL || `http://www.content.local/graphql`,
+        url: process.env.WPGRAPHQL_URL || `http://www.yourwpwebsite.local/graphql`,
       },
     },
     `gatsby-transformer-sharp`,
