56b371362742023d8737ccef74335aae3c5fd399
elliott-home-pc <elliott.wagener@mude.com.au>
2022-02-14 12:21:26 +1100

    Add dotenv to gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,14 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   plugins: [
     {
       resolve: `gatsby-source-wordpress`,
       options: {
-        url: process.env.WPGRAPHQL_URL || `http://www.yourwpwebsite.local/graphql`,
+        url:
+          process.env.WPGRAPHQL_URL || `http://www.yourwpwebsite.local/graphql`,
       },
     },
     `gatsby-transformer-sharp`,
